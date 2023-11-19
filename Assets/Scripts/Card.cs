850d7570cc91a8411d76cc26740c88fa487ce019
Bingergie <i@hebing.me>
2023-11-19 11:08:51 +0800

    added card.cs

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private TMP_Text attackText;
+    [SerializeField] private TMP_Text healthText;
+    [SerializeField] private TMP_Text costText;
+
+    [Header("Card Stats")] // todo: replace with scriptable object
+    [SerializeField] private int  attack;
+    [SerializeField] private int health;
+    [SerializeField] private int cost;
+    
+
+    
+    private void Start() {
+        // todo: set atk, hp, cost from scriptable object
+        
+        attackText.text = attack.ToString();
+        healthText.text = health.ToString();
+        costText.text = cost.ToString();
+    }
+
+    public void Attack(Card target) {
+        TakeDamage(target.attack);
+        target.TakeDamage(attack);
+    }
+    
+    protected void TakeDamage(int damage) {
+        health -= damage;
+        healthText.text = health.ToString();
+        if (health <= 0) {
+            Destroy(gameObject); // TODO: replace with proper death handling
+        }
+    }
+}
