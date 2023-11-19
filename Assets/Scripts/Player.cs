0177277b50571a1bad76c12282aa08add1c839f5
Bingergie <i@hebing.me>
2023-11-19 12:25:03 +0800

    gameplay works now! added Player class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInput), typeof(Camera))]
+public class Player : MonoBehaviour {
+    private Camera _camera;
+
+    [CanBeNull] private Card _selectedCard;
+
+    private void Awake() {
+        _camera = GetComponent<Camera>();
+        var input = GetComponent<PlayerInput>();
+        input.OnMouseDown += InputOnMouseDown;
+        input.OnMouseUp += InputOnMouseUp;
+        input.OnMouseMove += InputOnMouseMove;
+    }
+
+    private void InputOnMouseDown(object sender, Vector3 mousePosition) {
+        var ray = _camera.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out var hit, 1000)) {
+            var card = hit.collider.GetComponent<Card>();
+            if (card != null) {
+                _selectedCard = card;
+                Debug.Log("Selected card " + card.name);
+            }
+        }
+    }
+
+    private void InputOnMouseUp(object sender, Vector3 mousePosition) {
+        var ray = _camera.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out var hit)) {
+            var card = hit.collider.GetComponent<Card>();
+            if (_selectedCard != null && card != null && card != _selectedCard) {
+                _selectedCard.Attack(card);
+                Debug.Log("Attacked card " + card.name);
+            }
+        }
+    }
+
+    private void InputOnMouseMove(object sender, Vector3 mousePosition) {
+    }
+}
