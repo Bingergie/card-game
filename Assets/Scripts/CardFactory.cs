bc78d761899dc9426bb170ecfa54d0d6500de442
Bingergie <i@hebing.me>
2023-12-30 16:19:21 +0800

    added playerIndex argument to CreateCard

--- Assets/Scripts/CardFactory.cs
+++ Assets/Scripts/CardFactory.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 
 public static class CardFactory {
-    public static Card CreateCard(CardStats stats) {
+    public static Card CreateCard(CardStats stats, int playerIndex) {
         var gameObject = Object.Instantiate(Resources.Load<GameObject>("CardPrefabs/Card"));
         var card = gameObject.GetComponent<Card>();
+        card.playerIndex = playerIndex;
         card.SetStats(stats);
         return card;
     }
