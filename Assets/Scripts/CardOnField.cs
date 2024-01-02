c728fff3a42fe022094d24b9aecf5a2c99c1aab4
Bingergie <i@hebing.me>
2024-01-02 18:36:43 +0800

    renamed _card to CardObject and made it auto-property

--- Assets/Scripts/CardOnField.cs
+++ Assets/Scripts/CardOnField.cs
@@ -6,32 +6,33 @@ using UnityEngine.Serialization;
 [RequireComponent(typeof(DamageableEntity))]
 public class CardOnField : MonoBehaviour {
     public EventHandler OnCardDestroyed;
-    
+
     [SerializeField] private TMP_Text attackText;
     [SerializeField] private TMP_Text healthText;
     [SerializeField] private TMP_Text costText;
-    
+
     public int PlayerIndex { get; private set; }
-    public int Attack => _card.Attack;
-    public int MaxHealth => _card.MaxHealth;
+    public int Attack => CardObject.Attack;
+    public int MaxHealth => CardObject.MaxHealth;
     public int Health => _damageableEntity.Health;
-    public int Cost => _card.Cost;
-        
-    private Card _card;
+    public int Cost => CardObject.Cost;
+
+    public Card CardObject { get; private set; }
     private DamageableEntity _damageableEntity;
-    
+
     public static CardOnField CreateCard(Card cardObject, int playerIndex) {
         var gameObject = Instantiate(Resources.Load<GameObject>("CardPrefabs/Card"));
         var card = gameObject.GetComponent<CardOnField>();
         card.PlayerIndex = playerIndex;
-        card._card = cardObject;
+        card.CardObject = cardObject;
         return card;
     }
-    
+
     private void Awake() {
-        if (_card == null) {
-            _card = new Card(Resources.Load<CardData>("CardData/default"));
+        if (CardObject == null) {
+            CardObject = new Card(Resources.Load<CardData>("CardData/default"));
         }
+
         _damageableEntity = GetComponent<DamageableEntity>();
         _damageableEntity.OnDeath += HandleDeath;
         _damageableEntity.SetHealthToMax(MaxHealth);
@@ -44,7 +45,7 @@ public class CardOnField : MonoBehaviour {
         _damageableEntity.TakeDamage(damage);
         healthText.text = Health.ToString();
     }
-    
+
     private void HandleDeath(object sender, EventArgs e) {
         OnCardDestroyed?.Invoke(this, EventArgs.Empty);
         Destroy(gameObject); // TODO: replace with proper death handling
