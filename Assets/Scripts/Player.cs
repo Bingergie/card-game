2ce8e901be7d5a78a5855b5bda1e359a7bb1a84d
Bingergie <i@hebing.me>
2024-01-02 15:24:15 +0800

    revamped Player.cs to work using Card instead of CardData, renamed variables and methods.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,47 +2,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player {
-    public List<CardData> Deck;
+    public List<CardData> DeckList { get; private set; }
+    public readonly int PlayerIndex;
 
     private Deck _remainingDeck;
     private List<CardOnField> _hand = new List<CardOnField>();
     private List<CardOnField> _field = new List<CardOnField>();
 
-    public Player(List<CardData> deck) {
-        Deck = deck;
-        _remainingDeck = new Deck(deck);
+    public Player(List<CardData> deck, int playerIndex = 0) { // todo: remove " = 0"
+        DeckList = deck;
+        PlayerIndex = playerIndex;
+        _remainingDeck = new Deck(deck).Shuffle();
     }
 
     public void Attack(CardOnField attacker, CardOnField target) {
         GameController.Instance.HandleAttack(attacker, target);
     }
 
-    public CardData DrawCard() {
-        var card = _remainingDeck.Draw();
-        _hand.Add(CardFactory.CreateCard(card, 0));
+    public Card DrawCard() {
+        var card = _remainingDeck.DrawCard();
+        if (card != null) {
+            _hand.Add(CardOnField.CreateCard(card, PlayerIndex));
+        }
         return card;
     }
 }
 
 public class Deck {
-    private List<CardData> _cards;
+    private List<Card> _cards;
 
     public Deck(List<CardData> cards) {
-        _cards = cards;
-    }
-
-    public static void ShuffleDeck(Deck deck) {
-        var cards = deck._cards;
-        // Fisher-Yates shuffle
-        int n = cards.Count;
-        while (n > 1) {
-            n--;
-            int k = new System.Random().Next(n + 1);
-            (cards[k], cards[n]) = (cards[n], cards[k]);
+        _cards = new List<Card>(cards.Count);
+        foreach (var cardData in cards) {
+            _cards.Add(new Card(cardData));
         }
     }
 
-    public CardData Draw() {
+    public Deck Shuffle() {
+        // Fisher-Yates shuffle
+        int n = _cards.Count;
+        while (n > 1) {
+            n--;
+            int k = new System.Random().Next(n + 1);
+            (_cards[k], _cards[n]) = (_cards[n], _cards[k]);
+        }
+
+        return this;
+    }
+
+    public Card DrawCard() {
         if (_cards.Count <= 0) {
             Debug.Log("No more cards in deck!");
             return null; // or throw exception
